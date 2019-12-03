58e1b12a8c42a2ab22f0ce337e1682a77a36e2a5
ironzhou <1154117291@qq.com>
2019-12-03 17:01:04 +0800

    清空目录

--- README.md
+++
@@ -1,2 +0,0 @@
-# python-ml-exercise
-### python基础和机器学习基础学习代码
--- venv/Include/chat_robot.py
+++
@@ -1,7 +0,0 @@
-import aiml
-
-kernel = aiml.Kernel()
-kernel.learn('start_up.xml')
-kernel.respond('load chat robot')
-while True:
-    print(kernel.respond(input('Please enter you topic >>>')))
